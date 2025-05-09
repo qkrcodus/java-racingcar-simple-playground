03c00c6d33ece6b102574f764f905e3045e8dc11
qkrcodus <qkrcodus0722@gmail.com>
2025-05-09 14:55:05 +0900

    test : Car 클래스 단위 테스트 추가

---
+++ src/test/java/CarTest.java
@@ -0,0 +1,57 @@
+import car.Car;
+import generator.random.RandomGenerator;
+import move.decider.MoveDecider;
+import move.strategy.MoveStrategy;
+import move.strategy.OneStepMoveStrategy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CarTest {
+
+    @DisplayName("랜덤값이 조건을 만족할 경우 자동차는 1칸 전진한다")
+    @Test
+    void 움직일_수_있을_때_1칸_전진() {
+        // given
+        RandomGenerator fixedGenerator = () -> 7; // 7을 반환
+        MoveDecider alwaysTrueDecider = (number) -> true; // 항상 움직임
+        MoveStrategy strategy = new OneStepMoveStrategy(fixedGenerator, alwaysTrueDecider);
+        Car car = new Car("car1", strategy);
+
+        // when
+        car.move();
+
+        // then
+        assertThat(car.getMovedDistance()).isEqualTo(1);
+    }
+
+
+    @DisplayName("랜덤값이 조건을 만족하지 않을 경우 자동차는 전진하지 않는다")
+    @Test
+    void 움직일_수_없을_때_전진_하지_않음() {
+        RandomGenerator fixedGenerator = () -> 2;
+        MoveDecider alwaysFalseDecider = (number) -> false;
+        MoveStrategy strategy = new OneStepMoveStrategy(fixedGenerator, alwaysFalseDecider);
+        Car car = new Car("car2", strategy);
+
+        car.move();
+
+        assertThat(car.getMovedDistance()).isEqualTo(0);
+    }
+
+    @DisplayName("자동차는 여러 번 움직일 경우 이동 거리가 누적된다")
+    @Test
+    void 여러번_움직이면_누적됨() {
+        RandomGenerator fixedGenerator = () -> 8;
+        MoveDecider alwaysTrueDecider = (number) -> true;
+        MoveStrategy strategy = new OneStepMoveStrategy(fixedGenerator, alwaysTrueDecider);
+        Car car = new Car("car3", strategy);
+
+        car.move();
+        car.move();
+        car.move();
+
+        assertThat(car.getMovedDistance()).isEqualTo(3);
+    }
+}
