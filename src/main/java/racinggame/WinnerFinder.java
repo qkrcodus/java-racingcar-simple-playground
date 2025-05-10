499696127cfd715541cbe82f3f9809db8fc23192
qkrcodus <qkrcodus0722@gmail.com>
2025-05-10 16:08:54 +0900

    feat : 2단계 기능, 테스트 구현

---
+++ src/main/java/racinggame/WinnerFinder.java
@@ -0,0 +1,38 @@
+package racinggame;
+
+import car.Car;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class WinnerFinder {
+    private final List<Car> cars;
+    private final int moveCount;
+    public WinnerFinder(List<Car> cars, int moveCount){
+        this.cars = cars;
+        this.moveCount = moveCount;
+    }
+
+    public void play(){
+        for(int i=0;i<moveCount;i++){
+            moveAllCars();
+        }
+    }
+    public List<String> getWinners(){
+        int maxDistance=maxDistance();
+        return cars.stream().filter(car->car.getMovedDistance()==maxDistance).map(Car::getName).collect(Collectors.toList());
+    }
+
+    private void moveAllCars( ){
+        for (Car car : cars) {
+            car.move();
+        }
+    }
+    private int maxDistance(){
+        int max=0;
+        for (Car car : cars) {
+            max=Math.max(max,car.getMovedDistance());
+        }
+        return max;
+    }
+}
---
+++ src/test/java/WinnerFinderTest.java
@@ -0,0 +1,53 @@
+import car.Car;
+import car.move.strategy.MoveStrategy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racinggame.WinnerFinder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class WinnerFinderTest {
+
+    @Test
+    @DisplayName("우승자 1명일 경우 car1")
+    public void singleWinner(){
+        List<Car> cars = createCarsForTest(new int[]{3,2,1},new String[]{"car1","car2","car3"});
+        WinnerFinder winner=new WinnerFinder(cars,1);
+        winner.play();
+
+        List<String> winners = winner.getWinners();
+        assertThat(winners).containsExactly("car1");
+    }
+
+    @Test
+    @DisplayName("우승자 여러명일 경우 car1 car2")
+    public void multipleWinners(){
+        List<Car> cars = createCarsForTest(new int[]{3,3,1},new String[]{"car1","car2","car3"});
+        WinnerFinder winner=new WinnerFinder(cars,1);
+        winner.play();
+
+        List<String> winners = winner.getWinners();
+        assertThat(winners).containsExactly("car1", "car2");
+    }
+
+    // 이름과 이동 거리를 갖는 Car 리스트 생성하기
+    private List<Car> createCarsForTest(int[] distances,String[] names){
+        List<Car> cars = new ArrayList<>();
+        for (int i = 0; i < names.length; i++) {
+            cars.add(new Car(names[i],fixedStrategy(distances[i])));
+        }
+        return cars;
+    }
+
+    // 테스트 코드에만 존재하면 되므로 배포되지 않는 test 패키지안에 구현
+    // MoveStrategy 은 addStepSize( ) 하나의 메소드만 있다.
+    // 함수형 인터페이스를 사용한다.
+    private MoveStrategy fixedStrategy(int distance){
+        return ()->distance;
+    }
+
+}
