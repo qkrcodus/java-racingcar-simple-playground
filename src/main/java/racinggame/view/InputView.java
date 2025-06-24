f48c31237b5b19915aba9dcd49a8f2991a3b7b53
qkrcodus <qkrcodus0722@gmail.com>
2025-06-24 10:41:10 +0900

    fix : InputView 수정

--- src/main/java/racinggame/view/InputView.java
+++ src/main/java/racinggame/view/InputView.java
@@ -10,6 +10,8 @@ public class InputView {
     private static final String DELIMITER = ",";
     private static final String CAR_NAME_INPUT_PROMPT ="경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
 
+    private InputView(){};
+
     public static List<String> readCarNames() {
         Scanner scanner = new Scanner(System.in);
         System.out.println(CAR_NAME_INPUT_PROMPT);
